6d74dce1f575463c7464599e24aa7a0e85e852bf
akadam <akadam@radisys.com>
2024-09-23 23:30:06 +0530

    Managing API keys securely

--- weather/src/main/java/com/example/weather/Service/WeatherService.java
+++ weather/src/main/java/com/example/weather/Service/WeatherService.java
@@ -1,18 +1,20 @@
 package com.example.weather.Service;
 
 import com.example.weather.Model.WeatherResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
-    private final String API_KEY = "api_from_weatherApp";
+    @Value("${OPENWEATHER_API_KEY}")
+    private String apiKey;
 
     private final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
     public WeatherResponse getWeather(String city) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
-        String url = String.format("%s?q=%s&appid=%s", BASE_URL, city, API_KEY);
+        String url = String.format("%s?q=%s&appid=%s", BASE_URL, city, apiKey);
         return restTemplate.getForObject(url, WeatherResponse.class);
     }
 }
